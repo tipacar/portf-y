880a4b5570616dfa984d128c5cd840f51de59cc5
tipacar <egetipacar@gmail.com>
2024-09-15 14:19:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ def index():
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_SQL= request.form.get('button_SQL')
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html, button_SQL=button_SQL)
 
 
 if __name__ == "__main__":
